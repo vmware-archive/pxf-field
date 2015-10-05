f2e12c4dd657079621edc1aef32c4ef678c8ba8c
kojec <git@dbcoder.net>
2015-10-05 11:28:40 +0200

    add parameter for PXF_HOST

--- jdbc-pxf-ext/src/main/java/com/pivotal/pxf/plugins/jdbc/JdbcFragmenter.java
+++ jdbc-pxf-ext/src/main/java/com/pivotal/pxf/plugins/jdbc/JdbcFragmenter.java
@@ -14,30 +14,15 @@ import java.util.regex.Pattern;
  */
 public class JdbcFragmenter extends Fragmenter {
 
-    private String hostName;
-
     public JdbcFragmenter(InputData metaData) {
         super(metaData);
-
-        Pattern p = Pattern.compile("\\/\\/.+:");
-        Pattern p2 = Pattern.compile("\\/\\/.[^/:]+");
-
-        Matcher m = p.matcher(inputData.getUserProperty("DB_URL"));
-        Matcher m2 = p2.matcher(inputData.getUserProperty("DB_URL"));
-
-        if (m.find()) {
-            hostName = m.group(0).substring(2,m.group(0).length()-1);
-        }else if (m2.find()) {
-            hostName = m2.group(0).substring(2,m2.group(0).length());
-        }
-
     }
 
     @Override
     public List<Fragment> getFragments() throws Exception {
         List<Fragment> fragments = new ArrayList<Fragment>();
         fragments.add(new Fragment(
-                        inputData.getDataSource(),new String[]{hostName},new byte[0])
+                        inputData.getDataSource(),new String[]{inputData.getUserProperty("PXF_HOST")},new byte[0])
         );
         return fragments;
     }
